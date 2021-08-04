86b418be6ad2802e77728121b00be75d965f78c0
Douglas De León <dele18037@uvg.edu.gt>
2021-08-04 08:09:09 -0600

    Finished

--- miniproyecto.py
+++ miniproyecto.py
@@ -1,6 +1,22 @@
 from scipy.stats import uniform
 import matplotlib.pyplot as plt
+import numpy as np
 
-r = uniform.rvs(size=1000)
 
-print(r)
+N = 10000
+
+fig, ax = plt.subplots(1, 1)
+
+mean, var, skew, kurt = uniform.stats(moments='mvsk')
+print("Mu=", mean)
+r = uniform.rvs(size=N)
+
+ax.axhline(y=mean, color='r', label='Mu') # Mu line
+
+s_n = [ np.mean(r[:i]) for i in range(len(r)) ]
+ax.plot(s_n, label='n-->s_n')
+
+ax.set(title='Teoría de los grandes números', xlabel='n', ylabel='Media aritmética')
+ax.legend(loc='best')
+
+plt.show()
